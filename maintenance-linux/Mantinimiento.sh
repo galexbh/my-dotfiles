7a6143e41a85c0d8a7f010328be5724ff621c1b7
galexbh <galexbh@protonmail.com>
2021-01-01 17:32:04 -0600

    feat:autoclean, check, -foreground

--- maintenance-linux/Mantinimiento.sh
+++ maintenance-linux/Mantinimiento.sh
@@ -5,7 +5,7 @@
     # ! Mantenimiento
     # * Realizar un mantenimiento en SO linux Ubuntu
     # @author galexbh@protonmail.com
-    # @version 0.1
+    # @version 0.2
  
  ###
  
@@ -25,16 +25,17 @@
  
  clear
  
- setterm -background black # Terminal de color negro puede variar (red, blue, etc)
+ setterm -background black # setterm [ -background black|red|green|yellow|blue|magenta|cyan|white|default ]
+ setterm -foreground green # setterm [ -foreground black|red|green|yellow|blue|magenta|cyan|white|default ]
  
- if [["$(id -u)" !="0"]]; then # comprobar si se ejecuta como root
- echo "Este script debe ser ejecutado como root"
- sleep 3
- clear
+ if ! [ $(id -u) = 0 ]; then # comprobar si se ejecuta como root
+    echo "Este script debe ser ejecutado como root"
+    sleep 3
+    clear
  else
- echo "Este script sera ejecutado como SUPER ROOT"
- sleep 3
- clear
+    echo "Este script sera ejecutado como SUPER ROOT"
+    sleep 3
+    clear
  fi
  
 sudo apt update; sudo update-apt-xapian-index;
@@ -43,7 +44,8 @@ sudo apt --fix-broken install
 
 sudo update-grub; sudo update-grub2;
 sudo apt-get autoremove; sudo apt autoremove; 
-sudo apt purge; sudo apt remove
+sudo apt purge; sudo apt remove; sudo apt-get autoclean;
+sudo apt-get check
 
 sudo rm -f /var/log/*.old /var/log/*.gz /var/log/apt/* /var/log/auth* /var/log/daemon* /var/log/debug* /var/log/dmesg* /var/log/dpkg* /var/log/kern* /var/log/messages* /var/log/syslog* /var/log/user* /var/log/Xorg* /var/crash/* # remover logs
 
