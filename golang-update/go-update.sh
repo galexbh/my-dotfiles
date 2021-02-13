33e3773edafccc21f0c2b5def9b153eb3ee6a7a3
galexbh <galexbh@protonmail.com>
2021-02-13 10:42:39 -0600

    feat:up-golang

---
+++ golang-update/go-update.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+ 
+ echo Script para actualizar golang
+ 
+ sudo rm -rf /usr/local/go
+ 
+ curl -O https://dl.google.com/go/go1.15.8.linux-amd64.tar.gz # Cambiar la versión a lastest
+ 
+ tar -xvf go1.15.8.linux-amd64.tar.gz
+ sudo mv go /usr/local
+ 
+ export GOROOT=/usr/local/go
+
+ echo Finalizado con exito.
+ 
+ go --version
