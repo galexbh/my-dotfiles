8ba80e7a5bfcf04ba6b13479f6bc11ac5244a16f
galexbh <galexbh@protonmail.com>
2022-09-03 13:44:55 -0600

    feat(zsh): env

--- Arch_base/config/zsh/env.zsh
+++ Arch_base/config/zsh/env.zsh
@@ -13,6 +13,12 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
 # Golang
-export GOBIN=$HOME/go/bin
-export PATH=$PATH:$GOPATH:$GOBIN
+export GOBIN="$HOME/go/bin"
+export PATH="$PATH:$GOPATH:$GOBIN"
 export GO111MODULE=on
+
+# Flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# prevents duplicates PATH
+typeset -U PATH
